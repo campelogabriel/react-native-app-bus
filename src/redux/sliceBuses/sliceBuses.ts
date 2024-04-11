8a7abdfc399164ca8c4da0c6b20de0d68bb9495a
GabrielCampelo1998 <gabrielcampelo98@outlook.com>
2024-04-11 19:51:54 -0300

    refactor: improve some functionalities

--- src/redux/sliceBuses/sliceBuses.ts
+++ src/redux/sliceBuses/sliceBuses.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Bus } from "src/types/BusType";
+import { getDifference } from "src/utils/getDifference";
 import { getRotation } from "src/utils/getRotationBus";
 
 const initialStates: Bus[] | any | null = [];
@@ -12,31 +13,51 @@ const sliceBuses = createSlice({
       //Pegar o bus que tem o mesmo numero na carroceria
       let busOld = state.find((bus) => bus.ordem == payload.ordem);
 
+      let isLowerThan5Meters;
+      let isSamePosition;
+
+      //Verifica se a atualização foi menor que 10 metros
+      if (busOld) {
+        isLowerThan5Meters =
+          getDifference(
+            Number(busOld.latitude.replace(",", ".")),
+            Number(payload.latitude.replace(",", ".")),
+            Number(busOld.longitude.replace(",", ".")),
+            Number(payload.longitude.replace(",", "."))
+          ) < 0.01;
+
+        isSamePosition =
+          busOld.latitude == Number(payload.latitude.replace(",", ".")) &&
+          busOld.longitude == Number(payload.longitude.replace(",", "."));
+      }
+
       let busUpdated;
       //verifica se existe
       if (busOld) {
         busUpdated = {
           ordem: busOld.ordem,
           linha: busOld.linha,
-          count: busOld.count + 1,
+          count: isLowerThan5Meters ? busOld.count : busOld.count + 1,
           backgroundColor: busOld.backgroundColor,
           textColor: busOld.textColor,
           trajeto: busOld.trajeto,
-          latitude: payload.latitude,
-          longitude: payload.longitude,
+          latitude: isLowerThan5Meters ? busOld.latitude : payload.latitude,
+          longitude: isLowerThan5Meters ? busOld.longitude : payload.longitude,
           distanciaKm: payload.distanciaKm,
           velocidade: payload.velocidade,
           datahora: payload.datahora,
-          root: getRotation(
-            {
-              latitude: Number(busOld.latitude.replace(",", ".")),
-              longitude: Number(busOld.longitude.replace(",", ".")),
-            },
-            {
-              latitude: Number(payload.latitude.replace(",", ".")),
-              longitude: Number(payload.longitude.replace(",", ".")),
-            }
-          ),
+          root: isLowerThan5Meters
+            ? busOld.root
+            : getRotation(
+                {
+                  latitude: Number(busOld.latitude.replace(",", ".")),
+                  longitude: Number(busOld.longitude.replace(",", ".")),
+                },
+                {
+                  latitude: Number(payload.latitude.replace(",", ".")),
+                  longitude: Number(payload.longitude.replace(",", ".")),
+                }
+              ),
         };
 
         const newState = state.filter((bus) => bus.ordem !== payload.ordem);
--- src/utils/getRotationBus.ts
+++ src/utils/getRotationBus.ts
@@ -1,25 +1,25 @@
 import { Position } from "src/types/PositionType";
 
-// export const getRotation = (posAtual: Position, posFim: Position) => {
-//   if (!posAtual || !posFim) return 0;
+export const getRotation = (posAtual: Position, posFim: Position) => {
+  if (!posAtual || !posFim) return 0;
 
-//   const latDiff = posAtual.latitude - posFim.latitude;
-//   const lngDiff = posAtual.longitude - posFim.longitude;
-//   return (Math.atan2(lngDiff, latDiff) * 180) / Math.PI;
-// };
+  const latDiff = posAtual.latitude - posFim.latitude;
+  const lngDiff = posAtual.longitude - posFim.longitude;
+  return (Math.atan2(lngDiff, latDiff) * 180) / Math.PI;
+};
 
 // Nova forma de calcular rotation
-export const getRotation = (cord1: Position, cord2: Position) => {
-  if (cord2) {
-    const { latitude: lat1, longitude: lng1 } = cord1;
-    const { latitude: lat2, longitude: lng2 } = cord2;
-    const y = Math.sin(lng2 - lng1) * Math.cos(lat2);
-    const x =
-      Math.cos(lat1) * Math.sin(lat2) -
-      Math.sin(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1);
-    const θ = Math.atan2(y, x);
-    const brng = ((θ * 180) / Math.PI + 360) % 360;
-    return brng;
-  }
-  return 0;
-};
+// export const getRotation = (cord1: Position, cord2: Position) => {
+//   if (cord2) {
+//     const { latitude: lat1, longitude: lng1 } = cord1;
+//     const { latitude: lat2, longitude: lng2 } = cord2;
+//     const y = Math.sin(lng2 - lng1) * Math.cos(lat2);
+//     const x =
+//       Math.cos(lat1) * Math.sin(lat2) -
+//       Math.sin(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1);
+//     const θ = Math.atan2(y, x);
+//     const brng = ((θ * 180) / Math.PI + 360) % 360;
+//     return brng;
+//   }
+//   return 0;
+// };
